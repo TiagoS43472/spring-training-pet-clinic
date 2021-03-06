ddc1e7a1a794d02d6c02373ac962f16caa08becf
tstags.s@gmail.com <Tiago-1903>
2021-03-06 20:25:51 +0000

    spring initializer

---
+++ src/main/java/training/spring/springtrainingpetclinic/SpringTrainingPetClinicApplication.java
@@ -0,0 +1,13 @@
+package training.spring.springtrainingpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainingPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrainingPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/training/spring/springtrainingpetclinic/SpringTrainingPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package training.spring.springtrainingpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
